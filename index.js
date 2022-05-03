ed3cabf97ec19739069b9b4d1636cc6627bdb0b4
tretrauit <tretrauit@gmail.com>
2022-05-04 06:50:05 +0700

    feat: use try catch & log ip log ip so you can know which traffic is malicious or not

--- index.js
+++ index.js
@@ -6,14 +6,7 @@ const path = require('path')
 const port = process.env.PORT || 8080;
 const app = express()
 
-app.use(favicon(path.join(__dirname, 'favicon.ico')))
-app.get('/*', async (req, res) => {
-	const url = req.url.substring(1)
-	if (url.length === 0) {
-		res.send('Hello world!')
-		return
-	}
-	console.log("Got url:", url)
+async function fetchText(url) {
 	console.log("Launching puppeteer...")
 	const browser = await puppeteer.launch({
 		args: ['--no-sandbox', '--disable-setuid-sandbox']
@@ -22,11 +15,30 @@ app.get('/*', async (req, res) => {
 	console.log("Opening url...")
 	await page.goto(url)
 	const content = await page.$eval('*', (el) => el.innerText)
-	console.log("Returning content...")
-	res.send(content)
 	console.log("Closing browser...")
-	await browser.close();
+	await browser.close()
 	console.log("Done!")
+	return content
+}
+
+app.use(favicon(path.join(__dirname, 'favicon.ico')))
+app.get('/*', async (req, res) => {
+	console.log(`Got requrest from ${req.ip} (${req.ips})`)
+	const url = req.url.substring(1)
+	if (url.length === 0) {
+		res.send('Hello world!')
+		return
+	}
+	console.log("Got url:", url)
+	try {
+		const text = await fetchText(url)
+		console.log("Sending result...")
+		res.send(text)
+	} catch (err) {
+		console.log("Error:", err)
+		res.status(500)
+		res.send(err.message)
+	}
 });
 app.listen(port, function () {
 	console.log(`Running on port ${port}.`)
