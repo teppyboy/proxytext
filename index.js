7593cc89e061b638b56549f7b6ee6f2aab10b933
tretrauit <tretrauit@gmail.com>
2022-05-04 00:34:04 +0700

    feat: upload initial code

---
+++ index.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer');
+const express = require('express');
+
+const port = process.env.PORT || 8080;
+const app = express()
+
+app.get('/*', async (req, res) => {
+	const url = req.url.substring(1)
+	console.log("Got url:", url)
+	console.log("Launching puppeteer...")
+	const browser = await puppeteer.launch({})
+	const page = await browser.newPage()
+	console.log("Opening url...")
+	await page.goto(url)
+	const content = await page.$eval('*', (el) => el.innerText)
+	console.log("Returning content...")
+	res.send(content)
+	console.log("Closing browser...")
+	await browser.close();
+	console.log("Done!")
+});
+app.listen(port, function () {
+	console.log(`Running on port ${port}.`)
+});
